255346b5b59ed13b880b9921e22625be7a952e81
Nguyen Hai Dang <nyxethra@proton.me>
2024-11-17 19:37:59 +0700

    sửa giao diện

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Banner() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <div className='h-100 w-100'>
-      <img src="/assets/banner/bn1.jpg" className='h-100 w-100 border ' style={{ objectFit: "contain" }} alt="" />
+      <Slider {...settings}>
+        <div>
+          <img src="/assets/banner/bn1.jpg" className='h-100 w-100 border' style={{ objectFit: "contain" }} alt="Banner 1" />
+        </div>
+        <div>
+          <img src="/assets/banner/a.png" className='h-100 w-100 border' style={{ objectFit: "contain" }} alt="Banner 2" />
+        </div>
+        <div>
+          <img src="/assets/banner/b.jpg" className='h-100 w-100 border' style={{ objectFit: "contain" }} alt="Banner 3" />
+        </div>
+      </Slider>
     </div>
-  )
-}
+  );
+} 
