a3267d3dc012abfd814a2717a8ad914f432336ee
Nguyen Hai Dang <nyxethra@proton.me>
2024-11-17 15:10:35 +0700

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,32 @@
+
+// import { initializeApp } from "firebase/app";
+// import { getStorage } from "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBsEThGx3sz3-kfWbjSY3l0nSzg_JTO2Ps",
+//   authDomain: "imageupload-a6efc.firebaseapp.com",
+//   projectId: "imageupload-a6efc",
+//   storageBucket: "imageupload-a6efc.appspot.com",
+//   messagingSenderId: "214007906498",
+//   appId: "1:214007906498:web:0803ce76c5136276367b5b"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const imgDb=getStorage(app) 
+
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhe5JJMeIkrKTg3ln1RxSABJagC9jjbQ4",
+  authDomain: "democc-d09f7.firebaseapp.com",
+  projectId: "democc-d09f7",
+  storageBucket: "democc-d09f7.appspot.com",
+  messagingSenderId: "987973156196",
+  appId: "1:987973156196:web:3123422aa31c305ead4e71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const imgDb=getStorage(app) 
