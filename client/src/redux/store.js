9a21049932880e0e90a9428f0ea2bb4d3c07ae06
Lê Phương Lâm <144584119+luno-oui@users.noreply.github.com>
2024-11-21 10:55:15 +0700

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,7 +4,6 @@ import storage from 'redux-persist/lib/storage'
 import persistReducer from 'redux-persist/es/persistReducer' // lưu trữ trạng thái
 import persistStore from 'redux-persist/es/persistStore' // theo dõi trạng thái đã lưu
 
-
 const persisConfig={
     key:'root',
     version:1,
